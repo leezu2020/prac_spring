f5b3b278fa9bb593887563962f1d41c7624b3f80
leezu2020 <ejh7406@naver.com>
2021-01-11 12:33:49 +0900

    HomeController

---
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.newlecture.web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping("index")
+	@ResponseBody
+	public String index() {
+		//view 정보 전달
+		return "Hello Index";
+		// root.index로 tiles에서 resolving이 가능
+		//return "root.index";
+	}
+	
+	
+	/*
+	 * @Override public ModelAndView handleRequest(HttpServletRequest request,
+	 * HttpServletResponse response) throws Exception { // TODO Auto-generated
+	 * method stub
+	 * 
+	 * ModelAndView mv = new ModelAndView("root.index"); mv.addObject("data",
+	 * "Hello Spring MVC~"); //mv.setViewName("/WEB-INF/view/index.jsp"); return mv;
+	 * }
+	 */
+	
+}
